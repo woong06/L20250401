ba99a330dde6aa26849c340393f2141a96c6b660
woong06 <seongung7431@gmail.com>
2025-04-01 10:01:25 +0900

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,11 +4,21 @@ using namespace std;
 
 int main()
 {
-	cout << "+" << endl;
-	cout << "-" << endl;
-	cout << "*" << endl;
-	cout << "/" << endl;
-	cout << "%" << endl;
-	
+	int Number1 = 20;
+	int Number2 = 2;
+
+	cout << (Number1 + Number2) << endl;
+	cout << (Number1 - Number2) << endl;
+	cout << (Number1 * Number2) << endl;
+	cout << (Number1 / Number2) << endl;
+
+	float Number3 = 10.5f;
+	float Number4 = 2.0f;
+
+	cout << (Number3 + Number4) << endl;
+	cout << (Number3 - Number4) << endl;
+	cout << (Number3 * Number4) << endl;
+	cout << (Number3 / Number4) << endl;
+
 	return 0;
 }
